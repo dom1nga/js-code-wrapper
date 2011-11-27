1b67ab9b39f3ed3fe27b62cd66e195f6e842f9a8
dom1nga <beloved.dom1nga@gmail.com>
2011-11-27 22:25:46 +0200

    JsCodeWrapper implementation.

---
+++ lib/js-code-wrapper.rb
@@ -0,0 +1,42 @@
+module JsCodeWrapper
+  def wrap_js_code(text)
+    code_coords = code_tag_coords(text)
+    matches_count = script_to_code_tags_inclusion(text, code_coords).count {|t| !t}
+
+    while matches_count > 0 do
+      text = insert_code_tags(text, code_coords)
+      matches_count = matches_count - 1
+      code_coords = code_tag_coords(text)
+    end
+
+    text
+  end
+
+  private
+
+  def code_tag_coords(text)
+    code_begin = text.to_enum(:scan,/(<code)/i).map { |m,| $`.size }
+    code_end = text.to_enum(:scan,/(<\/code>)/i).map { |m,| $`.size }
+    0.upto(code_begin.length - 1).map {|i| [code_begin[i], code_end[i]] }
+  end
+
+  def script_to_code_tags_inclusion(text, code_coords)
+    text.to_enum(:scan,/(<script{1}[^<]*<\/script>{1})/i).map { |m,| $`.size }.map { |v| code_coords.map{|c| c[0] < v && v < c[1] }.compact.any? }
+  end
+
+  def insert_code_tags(text, code_coords)
+    matches = text.gsub /(<script{1}[^<]*<\/script>{1})/i
+
+    script_to_code_tags_inclusion(text, code_coords).each do |b|
+      tmp = matches.next
+      unless b
+        text[tmp] = '<code>'+ tmp +'</code>'
+        return text
+      end
+    end
+  end
+
+end
+
+Object.send(:include, JsCodeWrapper)
+ActionView::Base.send(:include, JsCodeWrapper) if defined?(ActionView::Base)
