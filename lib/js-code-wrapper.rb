bbf35636dd684d5e68933a740bfbec32dfb122af
Alexander Brodianoy <agb@anahoret.com>
2011-11-28 12:49:05 +0200

    Correct wrapping duplicated script in code.

--- lib/js-code-wrapper.rb
+++ lib/js-code-wrapper.rb
@@ -1,7 +1,7 @@
 module JsCodeWrapper
   def wrap_js_code(text)
     code_coords = code_tag_coords(text)
-    matches_count = script_to_code_tags_inclusion(text, code_coords).count {|t| !t}
+    matches_count = script_to_code_tags_inclusion(text, code_coords).count {|t| !t[1]}
 
     while matches_count > 0 do
       text = insert_code_tags(text, code_coords)
@@ -21,7 +21,7 @@ module JsCodeWrapper
   end
 
   def script_to_code_tags_inclusion(text, code_coords)
-    text.to_enum(:scan,/(<script{1}[^<]*<\/script>{1})/i).map { |m,| $`.size }.map { |v| code_coords.map{|c| c[0] < v && v < c[1] }.compact.any? }
+    text.to_enum(:scan,/(<script{1}[^<]*<\/script>{1})/i).map { |m,| $`.size }.map { |v| [v, code_coords.map{|c| c[0] < v && v < c[1] }.compact.any?] }
   end
 
   def insert_code_tags(text, code_coords)
@@ -29,8 +29,8 @@ module JsCodeWrapper
 
     script_to_code_tags_inclusion(text, code_coords).each do |b|
       tmp = matches.next
-      unless b
-        text[tmp] = '<code>'+ tmp +'</code>'
+      unless b[1]
+        text[b[0]..(b[0]+tmp.length-1)] = '<code>'+ tmp +'</code>'
         return text
       end
     end
--- spec/lib/js_code_wrapper_spec.rb
+++ spec/lib/js_code_wrapper_spec.rb
@@ -15,6 +15,11 @@ describe JsCodeWrapper do
       wrap_js_code('<script type="text/javascript" src="example.com/example.js"></script>').should == '<code><script type="text/javascript" src="example.com/example.js"></script></code>'
     end
 
+    it "should return the text with script body wrapped into code tags when script body duplicated in text" do
+      wrap_js_code('<code><script>alert("wrap me!")</script></code><script>alert("wrap me!")</script>').should ==
+      '<code><script>alert("wrap me!")</script></code><code><script>alert("wrap me!")</script></code>'
+    end
+
     it "should return the text with script body wrapped into code tags" do
       wrap_js_code("<code><script>alert('wrap me1!')</script><script type='text/javascript' src='example.com/example1.js'></script></code><p>text1</p><script>alert('wrap me2!')</script><script type='text/javascript' src='example.com/example2.js'></script><code><script>alert('wrap me3!')</script></code>").should == "<code><script>alert('wrap me1!')</script><script type='text/javascript' src='example.com/example1.js'></script></code><p>text1</p><code><script>alert('wrap me2!')</script></code><code><script type='text/javascript' src='example.com/example2.js'></script></code><code><script>alert('wrap me3!')</script></code>"
     end
