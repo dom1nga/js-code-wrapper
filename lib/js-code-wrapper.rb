19b645d361e1759a9babef602a7142a3b9a306e0
Бродяной Александр <beloved.dom1nga@gmail.com>
2011-12-06 14:41:40 +0200

    Added js escaping feature.

--- lib/js-code-wrapper.rb
+++ lib/js-code-wrapper.rb
@@ -1,9 +1,14 @@
 require 'nokogiri'
+require 'cgi'
 
 module JsCodeWrapper
-  def wrap_js_code(text)
+  def wrap_js_code(text, options = { :escape_js => false })
     doc = ::Nokogiri::HTML("<text>#{text}</text>")
-    doc.xpath("//script[not(ancestor::code)] | //*[@*[starts-with(name(), 'on')]]").wrap('<code>')
+    if options[:escape_js]
+      doc.xpath("//script[not(ancestor::code)] | //*[@*[starts-with(name(), 'on')]]").each {|elem| elem.replace(::CGI.escapeHTML(elem.to_html)) }
+    else
+      doc.xpath("//script[not(ancestor::code)] | //*[@*[starts-with(name(), 'on')]]").wrap('<code>')
+    end
     doc.xpath("//body/*").to_s.scan(/<text>(.*)<\/text>/im)[0][0]
   end
 end
