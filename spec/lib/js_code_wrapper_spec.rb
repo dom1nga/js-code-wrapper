bb97ec73dc71b91081d52e3679f74b60308b73e9
Бродяной Александр <beloved.dom1nga@gmail.com>
2011-12-06 15:03:29 +0200

    wrap_js_code as module function.

--- spec/lib/js_code_wrapper_spec.rb
+++ spec/lib/js_code_wrapper_spec.rb
@@ -4,47 +4,47 @@ describe JsCodeWrapper do
 
   describe "#wrap_js_code" do
     it "should return the text without changes, if it doesn't contains script tags" do
-      wrap_js_code("text <p>text</p><h1>text</h1>").should ==
+      JsCodeWrapper.wrap_js_code("text <p>text</p><h1>text</h1>").should ==
                    "text <p>text</p>\n<h1>text</h1>"
     end
 
     it "should return the text with script body (type1) wrapped into code tags" do
-      wrap_js_code("<script>alert('wrap me!')</script>").should ==
+      JsCodeWrapper.wrap_js_code("<script>alert('wrap me!')</script>").should ==
                    "<code><script>alert('wrap me!')</script></code>"
     end
 
     it "should return the text with script body (type2) wrapped into code tags" do
-      wrap_js_code('<script type="text/javascript" src="example.com/example.js"></script>').should ==
+      JsCodeWrapper.wrap_js_code('<script type="text/javascript" src="example.com/example.js"></script>').should ==
                    '<code><script type="text/javascript" src="example.com/example.js"></script></code>'
     end
 
     it "should return the text with script body wrapped into code tags when script body duplicated in text" do
-      wrap_js_code('<code><script>alert("wrap me!")</script></code><script>alert("wrap me!")</script>').should ==
+      JsCodeWrapper.wrap_js_code('<code><script>alert("wrap me!")</script></code><script>alert("wrap me!")</script>').should ==
                    '<code><script>alert("wrap me!")</script></code><code><script>alert("wrap me!")</script></code>'
     end
 
     it "should return the text with script body wrapped into code tags" do
-      wrap_js_code("<code><script>alert('wrap me1!')</script><script type='text/javascript' src='example.com/example1.js'></script></code><p>text1</p><script>alert('wrap me2!')</script><script type='text/javascript' src='example.com/example2.js'></script><code><script>alert('wrap me3!')</script></code>").should ==
+      JsCodeWrapper.wrap_js_code("<code><script>alert('wrap me1!')</script><script type='text/javascript' src='example.com/example1.js'></script></code><p>text1</p><script>alert('wrap me2!')</script><script type='text/javascript' src='example.com/example2.js'></script><code><script>alert('wrap me3!')</script></code>").should ==
                    "<code><script>alert('wrap me1!')</script><script type=\"text/javascript\" src=\"example.com/example1.js\"></script></code><p>text1</p>\n<code><script>alert('wrap me2!')</script></code><code><script type=\"text/javascript\" src=\"example.com/example2.js\"></script></code><code><script>alert('wrap me3!')</script></code>"
     end
 
     it "should return the text without changes, if script body into code tags" do
-      wrap_js_code("<code><script>alert('wrap me!')</script></code>").should ==
+      JsCodeWrapper.wrap_js_code("<code><script>alert('wrap me!')</script></code>").should ==
                    "<code><script>alert('wrap me!')</script></code>"
     end
 
     it "should return the text with link wrapped into code tags if link contains event attribute" do
-      wrap_js_code('<a href="#" onclick="alert(\'wrap me!\'); return 0;">link</a>').should ==
+      JsCodeWrapper.wrap_js_code('<a href="#" onclick="alert(\'wrap me!\'); return 0;">link</a>').should ==
                    "<code><a href=\"#\" onclick=\"alert('wrap me!'); return 0;\">link</a></code>"
     end
 
     it "should return the text with tags without changes if tags not contains event attributes" do
-      wrap_js_code('<a href="#">link</a><div class="div1"></div><p>text1</p>').should ==
+      JsCodeWrapper.wrap_js_code('<a href="#">link</a><div class="div1"></div><p>text1</p>').should ==
                    "<a href=\"#\">link</a><div class=\"div1\"></div>\n<p>text1</p>"
     end
     
     it "should return the text with escaped java script" do
-      wrap_js_code("<script>alert('wrap me!')</script>", :escape_js => true).should ==
+      JsCodeWrapper.wrap_js_code("<script>alert('wrap me!')</script>", :escape_js => true).should ==
                    "&lt;script&gt;alert('wrap me!')&lt;/script&gt;"
     end
 
